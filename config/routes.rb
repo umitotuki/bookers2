336db0662e6a482a8f21822ad163e875e16e72a1
umitotuki <umitukiuk@gmail.com>
2024-04-17 07:52:51 +0000

    update10

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   get 'homes/about' => 'homes#about' , as: 'about'
   devise_for :users
+  resources :books, only: [:new, :index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 end
