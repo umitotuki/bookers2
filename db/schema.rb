3b45655fca37c55c0fe8319f6f7ed75bac0962ab
umitotuki <umitukiuk@gmail.com>
2024-04-18 05:03:40 +0000

    update-16

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2024_04_17_062209) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.text "introductio"
+    t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
